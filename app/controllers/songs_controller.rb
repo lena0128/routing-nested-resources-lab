05cc291e964ed2298f942f05a3722d16245ad057
Lena Hou <lenahou2013@gmail.com>
2021-08-04 18:27:05 -0400

    commit tree 1b5b6cd6c98b1baf1db53ae60260df50322736ff

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,27 @@
 class SongsController < ApplicationController
+  
   def index
-    @songs = Song.all
+    if params[:artist_id] # when the user navigates to a specific artist
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil?
+        redirect_to artists_path, alert: "Artist not found"
+      else
+        @songs = @artist.songs
+        end
+      end
+      @songs = Song.all
   end
 
   def show
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      @song = @artist.songs.find_by(id: params[:id])
+        if @song.nil?
+          redirect_to artist_songs_path(@artist), alert: "Song not found"
+        end
+    else     
+      @song = Song.find(params[:id])
+    end
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
+  # Create a nested resource routes to show all songs to an artist => /artist/:id/songs
+# And individual songs for that artist => /artists/1/songs/1
+# Restrict the nested songs routes to `index` and `show` actions only
+
+resources :artists do
+  resources :songs, only: [:index, :show]
+end
+resources :songs
+
 end
